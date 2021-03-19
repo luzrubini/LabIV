357dd43d3d43e9f2d3c384c87d3cdc2b5f9a3e62
Luz Rubini <luzrubini@hotmail.com>
2021-03-18 21:17:34 -0300

    ejercicio uno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab-iv';
+  title = 'Ejercicio 1';
+  edad1 : number
+  edad2 : number;
+  suma : number;
+  promedio : number;
+  constructor(){
+    this.edad1 = 0
+    this.edad2 = 0
+    this.suma = 0
+    this.promedio =0
+  }
+  sumar()
+  {
+    this.suma = this.edad1 + this.edad2;
+    this.promedio = this.suma/2;
+  }
+  
 }
